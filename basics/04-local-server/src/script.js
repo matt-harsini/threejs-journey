937595c8bf0bd30afe9997d129f048b2edaa95e7
Matthew <matthewkimcs@gmail.com>
2023-04-09 20:05:40 -0700

    finished transforming objects section

--- basics/04-local-server/src/script.js
+++ basics/04-local-server/src/script.js
@@ -1,18 +1,28 @@
 import * as THREE from "three";
 
 const canvas = document.querySelector(".webgl");
-
 const scene = new THREE.Scene();
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({
-  color: "cyan",
-});
+const group = new THREE.Group();
+scene.add(group);
 
-const mesh = new THREE.Mesh(geometry, material);
-mesh.position.z = -3;
-console.log(mesh.position);
-scene.add(mesh);
+const cube1 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 2, 1),
+  new THREE.MeshBasicMaterial({ color: "cyan" })
+);
+
+const cube2 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: "green" })
+);
+
+cube2.position.x = -1;
+
+group.add(cube1);
+group.add(cube2);
+
+const axesHelper = new THREE.AxesHelper(3);
+scene.add(axesHelper);
 
 const sizes = {
   width: 800,
@@ -20,6 +30,7 @@ const sizes = {
 };
 
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.set(1, 1, 3);
 scene.add(camera);
 
 const renderer = new THREE.WebGLRenderer({
